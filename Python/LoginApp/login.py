fab3f5a6d2cc6ca9954aa39ee6c31deae9083586
Lucas Alexandre Zick <lucasalexandrezick@gmail.com>
2022-03-01 16:17:48 -0300

    Updated Login project

--- Python/LoginApp/login.py
+++ Python/LoginApp/login.py
@@ -1,4 +1,5 @@
 from PyQt5 import uic,QtWidgets
+import sqlite3
 
 def chamaTelaLogin():
     telaLogin.show()
@@ -17,19 +18,47 @@ def chamaTelaPrincipal(usuario):
 def login():
     login_usuario = telaLogin.lineEdit_login.text()
     senha_usuario = telaLogin.lineEdit_senha.text()
-    if login_usuario == 'lucaszick' and senha_usuario == '123456':
-        telaLogin.label_aviso.setText('Login realizado!')
-        chamaTelaPrincipal(login_usuario)
+    if senha_usuario != '' and login_usuario != '':
+        banco = sqlite3.connect('banco_cadastro.db')
+        cursor = banco.cursor()   
+        try:
+            cursor.execute(f"SELECT senha FROM cadastro WHERE login = '{login_usuario}'")
+            senha_bd = cursor.fetchall()
+            print(senha_bd[0][0])
+            banco.close()
+        except:
+            telaLogin.label_aviso.setText('Registro incompatível.')
     else:
-        telaLogin.label_aviso.setText(f'A conta {login_usuario} não existe\nou sua senha está incorreta.')
+        telaLogin.label_aviso.setText('Dados insuficientes para realizar o login.')
 
 def logout():
     telaPrincipal.close()
     telaLogin.show()
 
 def cadastrar():
-    telaRegistro.close()
-    telaLogin.show()
+    nome = telaRegistro.lineEdit_nome.text()
+    login = telaRegistro.lineEdit_loginCadastro.text()
+    senha = telaRegistro.lineEdit_senhaCadastro.text()
+    senhaConfirma = telaRegistro.lineEdit_senhaConfirma.text()
+
+    if (login != '' and nome != '' and senha == senhaConfirma):
+        try:
+            banco = sqlite3.connect('banco_cadastro.db')
+            cursor = banco.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS cadastro (nome text, login text, senha text)")
+            cursor.execute("INSERT INTO cadastro VALUES ('"+nome+"','"+login+"','"+senha+"')")
+
+            banco.commit()
+            banco.close
+
+            telaRegistro.close()
+            telaLogin.show()
+
+        except sqlite3 as erro:
+            telaRegistro.label_aviso.setText('Erro ao cadastrar o usuário.', erro)
+
+    else:
+        telaRegistro.label_aviso.setText('Cheque se preencheu todos os dados e reescreva as senhas de modo que fiquem iguais!')
 
 
 app = QtWidgets.QApplication([])
